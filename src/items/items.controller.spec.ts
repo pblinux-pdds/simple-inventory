c9d675507fe9c9fa44d1e39caa63d9f8ccea318d
Pablo Bautista <pablo@pixela.tech>
2025-07-29 20:02:52 -0600

    Develop (#4)
    * new: basic CRUD for item
    
    * change: dependencies and action for deploy

---
+++ src/items/items.controller.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ItemsController } from './items.controller';
+import { ItemsService } from './items.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { Repository } from 'typeorm';
+
+describe('ItemsController', () => {
+  let controller: ItemsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ItemsController],
+      providers: [
+        ItemsService,
+        {
+          provide: getRepositoryToken(Item),
+          useValue: Repository
+        }
+      ],
+    }).compile();
+
+    controller = module.get<ItemsController>(ItemsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
